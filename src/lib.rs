9aeb0b8f28acc89bb4c5983b93852b84c34af59d
Zynh0722 <Zynh0722@gmail.com>
2023-10-03 14:20:05 -0700

    oh boy, abstraction pain

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ use std::cell::RefCell;
 use std::collections::{hash_map::Entry, HashMap};
 
 use log::*;
-use screeps::ConstructionSite;
 use screeps::{
     constants::{ErrorCode, Part, ResourceType},
     enums::StructureObject,
@@ -13,6 +12,7 @@ use screeps::{
     objects::{Creep, Source, StructureController},
     prelude::*,
 };
+use screeps::{ConstructionSite, RoomObject, Structure, StructureExtension, StructureSpawn};
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
@@ -30,6 +30,22 @@ thread_local! {
     static CREEP_TARGETS: RefCell<HashMap<String, CreepTarget>> = RefCell::new(HashMap::new());
 }
 
+// trait HasStore {
+//     fn store(&self) -> screeps::objects::Store;
+// }
+//
+// impl HasStore for StructureSpawn {
+//     fn store(&self) -> screeps::objects::Store {
+//         self.store()
+//     }
+// }
+//
+// impl HasStore for StructureExtension {
+//     fn store(&self) -> screeps::objects::Store {
+//         self.store()
+//     }
+// }
+
 // this enum will represent a creep's lock on a specific target object, storing a js reference
 // to the object id so that we can grab a fresh reference to the object each successive tick,
 // since screeps game objects become 'stale' and shouldn't be used beyond the tick they were fetched
@@ -39,7 +55,61 @@ enum CreepTarget {
     Upgrade(ObjectId<StructureController>),
     Harvest(ObjectId<Source>),
     Construct(ObjectId<ConstructionSite>),
+    Store(StoreTarget),
 }
+
+#[derive(Clone, Debug, Serialize)]
+enum StoreTarget {
+    Extension(ObjectId<StructureExtension>),
+    Spawn(ObjectId<StructureSpawn>),
+}
+
+impl StoreTarget {
+    fn resolve(&self) -> Option<ResolvedStoreTarget> {
+        match self {
+            StoreTarget::Extension(id) => match id.resolve() {
+                Some(structure) => Some(ResolvedStoreTarget::Extension(structure)),
+                None => None,
+            },
+            StoreTarget::Spawn(id) => match id.resolve() {
+                Some(structure) => Some(ResolvedStoreTarget::Spawn(structure)),
+                None => None,
+            },
+        }
+    }
+}
+
+#[derive(Clone, Debug, Serialize)]
+enum ResolvedStoreTarget {
+    #[serde(skip)]
+    Extension(StructureExtension),
+    #[serde(skip)]
+    Spawn(StructureSpawn),
+}
+
+impl HasStore for ResolvedStoreTarget {
+    fn store(&self) -> screeps::Store {
+        use ResolvedStoreTarget::*;
+
+        match self {
+            Extension(structure) => structure.store(),
+            Spawn(structure) => structure.store(),
+        }
+    }
+}
+
+impl AsRef<RoomObject> for ResolvedStoreTarget {
+    fn as_ref<'a>(&'a self) -> &'a RoomObject {
+        use ResolvedStoreTarget::*;
+
+        match self {
+            Extension(structure) => structure.as_ref(),
+            Spawn(structure) => structure.as_ref(),
+        }
+    }
+}
+
+impl Transferable for ResolvedStoreTarget {}
 
 #[derive(Deserialize, Debug)]
 struct Memory {
@@ -53,11 +123,11 @@ pub fn game_loop() {
     let starting_time = game::cpu::get_used();
     let current_tick = game::time();
 
-    // if current_tick % 10 == 0 {
-    //     CREEP_TARGETS.with_borrow(|ct_refcell| {
-    //         info!("CREEP_TARGETS: {:#?}", ct_refcell);
-    //     });
-    // }
+    if current_tick % 10 == 0 {
+        CREEP_TARGETS.with_borrow(|ct_refcell| {
+            info!("CREEP_TARGETS: {:#?}", ct_refcell);
+        });
+    }
 
     if current_tick % 60 == 0 {
         use js_sys::Reflect;
@@ -205,6 +275,25 @@ fn run_creep(creep: &Creep, creep_targets: &mut HashMap<String, CreepTarget>) {
                         entry.remove();
                     }
                 }
+                CreepTarget::Store(source) => {
+                    if let Some(source) = source.resolve() {
+                        if creep.pos().is_near_to(source.pos()) {
+                            creep
+                                .transfer(&source, ResourceType::Energy, None)
+                                .unwrap_or_else(|e| {
+                                    warn!("couldn't transfer: {:?}", e);
+                                    entry.remove();
+                                })
+                        } else {
+                            let _ = creep.move_to_with_options(
+                                &source,
+                                Some(screeps::MoveToOptions::new().reuse_path(5)),
+                            );
+                        }
+                    } else {
+                        entry.remove();
+                    }
+                }
                 _ => {
                     entry.remove();
                 }
@@ -215,18 +304,54 @@ fn run_creep(creep: &Creep, creep_targets: &mut HashMap<String, CreepTarget>) {
             let room = creep.room().expect("couldn't resolve creep room");
             'temp: {
                 if creep.store().get_used_capacity(Some(ResourceType::Energy)) > 0 {
+                    // if controller needs a timer reset, fill it
                     for structure in room.find(find::STRUCTURES, None).iter() {
                         if let StructureObject::StructureController(controller) = structure {
-                            if controller.ticks_to_downgrade() < 5000 {
+                            if controller.ticks_to_downgrade() < 7500 {
                                 entry.insert(CreepTarget::Upgrade(controller.id()));
                                 break 'temp;
                             }
                         }
                     }
 
+                    // build things
                     for site in room.find(find::CONSTRUCTION_SITES, None) {
                         if let Some(id) = site.try_id() {
                             entry.insert(CreepTarget::Construct(id));
+                            break 'temp;
+                        }
+                    }
+
+                    // fill extensions
+                    for structure in room.find(find::STRUCTURES, None).iter() {
+                        if let StructureObject::StructureExtension(extension) = structure {
+                            if extension
+                                .store()
+                                .get_free_capacity(Some(ResourceType::Energy))
+                                > 0
+                            {
+                                entry.insert(CreepTarget::Store(StoreTarget::Extension(
+                                    extension.id(),
+                                )));
+                                break 'temp;
+                            }
+                        }
+                    }
+
+                    // fill spawners
+                    for structure in room.find(find::STRUCTURES, None).iter() {
+                        if let StructureObject::StructureSpawn(spawn) = structure {
+                            if spawn.store().get_free_capacity(Some(ResourceType::Energy)) > 0 {
+                                entry.insert(CreepTarget::Store(StoreTarget::Spawn(spawn.id())));
+                                break 'temp;
+                            }
+                        }
+                    }
+
+                    // default case, upgrade controller
+                    for structure in room.find(find::STRUCTURES, None).iter() {
+                        if let StructureObject::StructureController(controller) = structure {
+                            entry.insert(CreepTarget::Upgrade(controller.id()));
                             break 'temp;
                         }
                     }
